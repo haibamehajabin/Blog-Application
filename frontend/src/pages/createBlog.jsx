ec6e6eb1e9b90e7a911ddace3deec4497b80e94c
haibamehajabin <164992428+haibamehajabin@users.noreply.github.com>
2024-04-20 00:55:53 -0700

    Add files via upload

---
+++ frontend/src/pages/createBlog.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import newForm from '../components/Form/newForm';
+const createBlog = () => {
+  return (
+    <div>
+    <newForm/>
+ </div>)
+}
+export default createBlog;
